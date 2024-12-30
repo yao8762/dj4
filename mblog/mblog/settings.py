70074b3bdfead7d2d8f6625160b34bc2858bf93b
yao8762 <hot665992002@gmail.com>
2024-12-31 02:36:04 +0800

    set settings.py

--- mblog/mblog/settings.py
+++ mblog/mblog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-0vs4svrk&54%r1y*!!^9*kp5%hpejhqx$w4xe$=*@g$$h@n47g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "mysite",
 ]
 
 MIDDLEWARE = [
@@ -103,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "zh-hans"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Asia/Taipei"
 
 USE_I18N = True
 
